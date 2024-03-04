ff0a7b679f2abfcd1892037e508d3c5be40802ec
Jenson0411 <94308520+Jenson0411@users.noreply.github.com>
2024-03-04 09:34:11 -0500

    add dat gui control for shaking animation

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import * as THREE from 'three';
+import { GUI } from 'dat.gui';
 
 // Create a scene
 const scene = new THREE.Scene();
 
 // Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 12);
+camera.lookAt(0, 0, 0);
+
 
 // Create a renderer
 const renderer = new THREE.WebGLRenderer();
@@ -12,67 +16,95 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 renderer.setClearColor(0xADD8E6);
 
-// Create a sphere
+// Create a top of the snowglobe
 const radius = 4;
 const theta = -Math.PI / 4;
-
-const geometry = new THREE.SphereGeometry(radius, 64, 64, 0, Math.PI * 2, theta, Math.PI);
-const material = new THREE.MeshPhysicalMaterial({
-    color: "white",
+const textureLoader = new THREE.TextureLoader();
+const snowTexture = textureLoader.load("textures/snowwall.jpg")
+const topGeometry = new THREE.SphereGeometry(radius, 64, 64, 0, Math.PI * 2, theta, Math.PI);
+const glassMaterial = new THREE.MeshPhysicalMaterial({
     roughness : 0,
     metalness: 0,
     transmission: 1,
 });
 
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
+const top = new THREE.Mesh(topGeometry, glassMaterial);
+scene.add(top);
 
-// Calculation for cylinder position
-const capRadius = radius * Math.sin(Math.abs(theta));
+// Calculation for spherical cap radius(basic trigonometry)
+const baseRadius = radius * Math.sin(Math.abs(theta));
 
-// Create a green cylinder
-const cylinderGeometry = new THREE.CylinderGeometry(capRadius, capRadius+1, 2);
-const cylinderMaterial = new THREE.MeshBasicMaterial({ color: "green" });
-const cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
-scene.add(cylinder);
 
-// Set position of the cylinder
-cylinder.position.set(0, -radius, 0);
+// Create base of snowglobe
+const woodTexture = textureLoader.load("textures/OIP.jpg")
 
-// Set camera position and orientation
-camera.position.set(0, 0, 12);
-camera.lookAt(0, 0, 0);
+const baseGeometry = new THREE.CylinderGeometry(baseRadius, baseRadius*1.5, 2);
+const baseMaterial = new THREE.MeshBasicMaterial({ map : woodTexture});
+const base = new THREE.Mesh(baseGeometry, baseMaterial);
+scene.add(base);
+base.position.set(0, -radius, 0);
 
-// Add lights
+// Ambient lights
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
 scene.add(ambientLight);
 
+// Directional Light
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(5, 5, 5);
 scene.add(directionalLight);
-var shakingDirection = "L"
+
+const shakingVariables = {
+    speed : 1,
+    animationFrames : 100
+
+}
+
+//Dat Slider
+const gui = new GUI();
+gui.addFolder("Shaking Controls");
+var shakeAnimationFolder = gui.addFolder('Shaking Setting');
+shakeAnimationFolder.add(shakingVariables, 'speed', 0, 3, 0.1).name("Speed");
+shakeAnimationFolder.add(shakingVariables, 'animationFrames', 0, 500, 1).name("Animation Frame");
+shakeAnimationFolder.open();
+
+gui.add({'Shake Animation': shakeAnimation}, 'Shake Animation').name("Shake Animation");
+
+
+
+function shakeAnimation(){
+    t = 0;
+}
+
+
+var t = 100;
+var animationFrames = 100
+var speed = 1;
+
+var shakingDirection = "U";
 // Animation loop
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    if(t<shakingVariables.animationFrames){
+        if(shakingDirection=="D"){
+            top.position.y = top.position.y - 0.5 *shakingVariables.speed;
+            base.position.y = base.position.y -0.5 * shakingVariables.speed;
+            
+        }
+        else if(shakingDirection =="U"){
+            top.position.y = top.position.y + 0.5 *shakingVariables.speed;
+            base.position.y = base.position.y + 0.5*shakingVariables.speed;
 
-    if(shakingDirection=="L"){
-         sphere.position.x = sphere.position.x - 0.5;
-         cylinder.position.x = cylinder.position.x -0.5;
-         count=  count+1;
-    }
-    else if(shakingDirection =="R"){
-        sphere.position.x = sphere.position.x + 0.5;
-        cylinder.position.x = cylinder.position.x + 0.5;
-        count=  count+1;
-   }
+        }
 
-   if(sphere.position.x < -3 && shakingDirection == "L"){
-       shakingDirection = "R";
-   }
-   else if(sphere.position.x > 3 && shakingDirection == "R"){
-       shakingDirection = "L";
-   }
+        if(top.position.y < -0.5 && shakingDirection == "D"){
+            shakingDirection = "U";
+        }
+        else if(top.position.y > 0.5 && shakingDirection == "U"){
+            shakingDirection = "D";
+        }
+        t = t + 1;
+    }    
 
 }
 animate();
