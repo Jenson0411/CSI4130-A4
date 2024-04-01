aae0a45021e49deab9c41cd92ec0f5097bc8e81b
Jenson0411 <jensonwu81@gmail.com>
2024-04-01 19:31:56 -0400

    adjustment to the formatting

--- index.js
+++ index.js
@@ -7,11 +7,30 @@ var animationFrames = 100;
 var speed = 1;
 var shakingDirection = "U";
 var shakeState = false;
+var snowfallStarted = false;
+var snowfallTimer = 0;
+const snowfallDuration = 10;
+var deathCounter = 0
+var count = 0;
+var flag2 = true;
+
+// Create snow particles
+const particleCount = 500;
+const particles = new THREE.BufferGeometry();
+const positions = new Float32Array(particleCount * 3); // 3 values per vertex (x, y, z)
+const velocities = new Float32Array(particleCount * 3); // 3 values per velocity (vx, vy, vz)
+const deaths = new Float32Array(particleCount);
+const state = new Float32Array(particleCount);
+const gravity = new THREE.Vector3(0, -0.005, 0); // Adjust gravity as needed
+
 // Create a scene
 const scene = new THREE.Scene();
 
 // Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// Setting up camera
+camera.position.set(0, 0, 12);
 
 const shakingVariables = {
     speed: 1,
@@ -25,10 +44,13 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 renderer.setClearColor(0xADD8E6);
 
+
 // Create a top of the snowglobe
 const radius = 6;
 const theta = -Math.PI / 4;
 const textureLoader = new THREE.TextureLoader();
+const snowflakeTexture = textureLoader.load('textures/snowflake.png');
+
 const topGeometry = new THREE.SphereGeometry(radius, 64, 64, 0, Math.PI * 2, theta, Math.PI);
 const glassMaterial = new THREE.MeshPhysicalMaterial({
     roughness: 0,
@@ -39,8 +61,6 @@ const glassMaterial = new THREE.MeshPhysicalMaterial({
 const top = new THREE.Mesh(topGeometry, glassMaterial);
 scene.add(top);
 
-// Setting up camera
-camera.position.set(0, 0, 12);
 
 // Calculation for spherical cap radius(basic trigonometry)
 const baseRadius = radius * Math.sin(Math.abs(theta));
@@ -55,19 +75,21 @@ const base = new THREE.Mesh(baseGeometry, baseMaterial);
 scene.add(base);
 base.position.set(0, -((radius - capHeight) + baseHeight / 2), 0);
 
+// creating the snow layer on ground
 const snowGeometry = new THREE.CylinderGeometry(baseRadius, baseRadius, 0.000000000000001);
 const snowMaterial = new THREE.MeshBasicMaterial({color: 'white'})
 const snowLayer =  new THREE.Mesh(snowGeometry, snowMaterial)
 snowLayer.position.set(0, -((radius - capHeight +0.000000000000001/2)), 0);
 scene.add(snowLayer);
 
+//Creating the base of the christmas tree
 const treeBaseGeometry = new THREE.CylinderGeometry(1, 1, 3);
 const treeBaseMaterial = new THREE.MeshBasicMaterial({color: 'brown'})
 const treeBase =  new THREE.Mesh(treeBaseGeometry, treeBaseMaterial);
 treeBase.position.set(0, -((radius - capHeight)) + 3/2, 0);
 scene.add(treeBase);
 
-
+// Creating the top of the christmas tree
 const treeHeight = 3
 const treeCones = new THREE.ConeGeometry(2, 3);
 const treeConeMaterial = new THREE.MeshBasicMaterial({color: 'green'})
@@ -81,6 +103,7 @@ scene.add(treeCone1);
 scene.add(treeCone2);
 scene.add(treeCone3);
 
+
 // Ambient lights
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
 scene.add(ambientLight);
@@ -89,6 +112,7 @@ scene.add(ambientLight);
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(5, 5, 5);
 scene.add(directionalLight);
+
 
 //Dat Slider
 const gui = new GUI();
@@ -103,24 +127,10 @@ shakeAnimationFolder.add(shakingVariables, 'speed', 0, 3, 0.1).name("Speed");
 shakeAnimationFolder.add(shakingVariables, 'animationFrames', 0, 500, 1).name("Animation Frame");
 shakeAnimationFolder.open();
 
-var snow
-
 gui.add({ 'Shake Animation': shakeAnimation }, 'Shake Animation').name("Shake Animation");
 
-// Create snow particle texture
-const snowflakeTexture = textureLoader.load('textures/snowflake.png');
-
-// Create snow particles
-const particleCount = 500;
-const particles = new THREE.BufferGeometry();
-const positions = new Float32Array(particleCount * 3); // 3 values per vertex (x, y, z)
-const velocities = new Float32Array(particleCount * 3); // 3 values per velocity (vx, vy, vz)
-const deaths = new Float32Array(particleCount)
-const state = new Float32Array(particleCount)
-const gravity = new THREE.Vector3(0, -0.005, 0); // Adjust gravity as needed
-
+// Initialize velocity, position and states of each particle
 function particleSystemInit(){
-
     for (let i = 0; i < particleCount; i++) {
         let coord = generateRandomPointInsideSphere();
 
@@ -132,9 +142,9 @@ function particleSystemInit(){
         const vz = Math.random() * 0.02 -0.01; // Random velocity in z direction
 
         // Set particle position
-        positions[i * 3] = 0;
-        positions[i * 3 + 1] =  radius + 2;
-        positions[i * 3 + 2] = 0;
+        positions[i * 3] = x;
+        positions[i * 3 + 1] =  y;
+        positions[i * 3 + 2] = z;
 
         // Set particle velocity
         velocities[i * 3] = vx;
@@ -143,7 +153,7 @@ function particleSystemInit(){
         
         //set particile's death counter
         deaths[i] = 0;
-        state[i] = 0;
+        state[i] = 1;
     }
 
     particles.setAttribute('position', new THREE.BufferAttribute(positions, 3));
@@ -169,17 +179,6 @@ const particleSystem = new THREE.Points(particles, particleMaterial);
 
 
 
-// Animation loop
-// Flag to indicate whether the snowfall animation has started
-let snowfallStarted = false;
-// Time elapsed since the end of shaking animation
-let snowfallTimer = 0;
-// Duration of snowfall animation in seconds
-const snowfallDuration = 10;
-var deathCounter = 0
-particleSystemInit();
-var count =0;
-var flag = false;
 // Function to start the shaking animation
 function shakeAnimation() {
     t = 0;
@@ -189,7 +188,9 @@ function shakeAnimation() {
     snowfallStarted = false;
     snowfallTimer = 0;
 }
-var flag2 = true;
+
+
+
 // Animation loop
 function animate() {
     requestAnimationFrame(animate);
@@ -228,17 +229,11 @@ function animate() {
             snowfallStarted = true;
             shakeState = false; // Reset shake state
             scene.add(particleSystem); // Add snow particles to the scene
-            flag2= true;
+            particleSystemInit();
         }
     } else if (snowfallStarted) {
         // Snowfall animation
         if (deathCounter<particleCount) { // Convert duration to frames
-            if(flag2){
-                for(var i = count ; i<=particleCount; i+=10){
-                    setInterval(generateSnowparticle, 100*i);
-                }
-                flag2 = false;
-            }    
             snowFalling();
         }    
          else {
@@ -247,6 +242,7 @@ function animate() {
             snowfallStarted = false;
             scene.remove(particleSystem);
             particleSystemInit();
+            count = 0;
         }
     }
 }
@@ -265,51 +261,6 @@ function generateRandomPointInsideSphere() {
     }    
 }
 
-function generateSnowparticle(timeout){
-    const positions = particles.getAttribute('position');
-    const velocities = particles.getAttribute('velocity');
-    const deaths = particles.getAttribute('death');
-    const states = particles.getAttribute('state');
-    console.log(timeout);
-    for (let i = count; i < count+10; i++) {
-        if(count<particleCount){
-            let coord = generateRandomPointInsideSphere();
-
-            const y = coord[1]
-            const x = coord[0]
-            const z = coord[2]
-            const vx = Math.random() * 0.02 - 0.01 // Random velocity in x direction
-            const vy = Math.random() * 0.02 - 0.01; // Random velocity in y direction
-            const vz = Math.random() * 0.02 -0.01; // Random velocity in z direction
-
-            // Set particle position
-            positions[i * 3] = x;
-            positions[i * 3 + 1] =  y;
-            positions[i * 3 + 2] = z;
-
-            // Set particle velocity
-            velocities[i * 3] = vx;
-            velocities[i * 3 + 1] = vy;
-            velocities[i * 3 + 2] = vz;
-            
-            //set particile's death counter
-            deaths[i] = 0;
-            state[i] = 1;
-            count++;
-        }    
-    }
-
-    particles.setAttribute('position', positions);
-    particles.setAttribute('velocity', velocities);
-    particles.setAttribute('death', deaths);
-    particles.setAttribute('state', states);
-
-    particleSystem.geometry.attributes.position.needsUpdate = true; 
-    particleSystem.geometry.attributes.velocity.needsUpdate = true; 
-    particleSystem.geometry.attributes.death.needsUpdate = true;
-    particleSystem.geometry.attributes.state.needsUpdate = true;
-
-}
 
 
 function snowFalling(){
@@ -369,7 +320,6 @@ function snowFalling(){
                 velocities.array[i * 3 + 2] = Math.random() * 0.02 -0.01; // Random velocity in z directio
                 
             }
-            flag = false;
             if(deaths.array[i] == 21){
                 deathCounter++;
                 positions.array[i * 3 ] = 0;
